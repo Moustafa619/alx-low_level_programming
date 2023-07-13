b89118d6f2e62f2f0bb1890daae88c2c5f507c27
Moustafa Ibrahim <sasaelmobd3@gmail.com>
2023-07-13 16:28:50 -0700

    5 last update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void) 
+/**
+* main - Entry point
+*
+* Return: 0 on success
+*/
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\"");
 return (0);
 }
