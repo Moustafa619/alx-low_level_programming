14d182aaadcb1e850db728f029c2346765f94093
Moustafa Ibrahim <sasaelmobd3@gmail.com>
2023-07-14 16:37:03 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n == 0)
 {
-printf("the number is 0")
+printf("the number is 0");
 }
 else if (n > 0)
 {
-printf("the number is positive")
+printf("the number is positive");
 }
 else if (n < 0)
 {
-printf("the number is 0")
+printf("the number is 0");
 }
 return (0);
 }
