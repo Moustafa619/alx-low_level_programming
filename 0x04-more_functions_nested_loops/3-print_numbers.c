605ba094cc6edb2d15d04875c270f74eeec176c2
Moustafa Ibrahim <sasaelmobd3@gmail.com>
2023-07-20 19:27:51 -0700

    '3'

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_numbers - print numbers
+*/
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+}
