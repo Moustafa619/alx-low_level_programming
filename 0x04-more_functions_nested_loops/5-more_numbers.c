7905686f537d18d2e3d6417d9710039afb336867
Moustafa Ibrahim <sasaelmobd3@gmail.com>
2023-07-20 19:29:56 -0700

    '5'

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* more_numbers - print numbers
+*/
+void more_numbers(void)
+{
+int i, j, k, l = 9;
+
+for (j = 0; j < 10; j++)
+{
+for (k = 0; k <= 1; k++)
+{
+if (k == 1)
+{
+l = 4;
+}
+for (i = 0; i <= l; i++)
+{
+if (k == 1)
+{
+_putchar(1 + '0');
+}
+_putchar(i + '0');
+}
+}
+_putchar('\n');
+}
+}
